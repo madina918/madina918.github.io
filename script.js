8765848af02e6f7f0875639b5ae91afe0f7e6424
Madina <imomova22@gmail.com>
2024-08-19 01:44:05 -0400

    created some changes in html css and js files

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
+// script.js
+
 const homeworks = [
     { title: "Homework 1", link: "path/to/homework1" },
     { title: "Homework 2", link: "path/to/homework2" },
-    { title: "Homework 3", link: "path/to/homework3" },
-   
+    { title: "Homework 3", link: "path/to/homework3" }
+    // Add more homework objects as needed
 ];
 
-
-const homeworkList = document.querySelector(".homeworks ul");
-
+const homeworkList = document.querySelector("#homeworks ul");
+homeworkList.innerHTML = ""; // Clears any existing content
 
 homeworks.forEach(homework => {
     const li = document.createElement("li");
@@ -18,3 +19,5 @@ homeworks.forEach(homework => {
     li.appendChild(a);
     homeworkList.appendChild(li);
 });
+console.log("Script is running");
+
