86492198f0a62db97285100de6e86b99ceee260a
Madina <imomova22@gmail.com>
2024-08-19 21:31:09 -0400

    Added homework folder and linked to homework.html

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 
-// script.js
-
 const homeworks = [
     { title: "Homework 1", link: "path/to/homework1" },
     { title: "Homework 2", link: "path/to/homework2" },
     { title: "Homework 3", link: "path/to/homework3" }
-    // Add more homework objects as needed
+   
 ];
 
 const homeworkList = document.querySelector("#homeworks ul");
-homeworkList.innerHTML = ""; // Clears any existing content
+homeworkList.innerHTML = "";
 
 homeworks.forEach(homework => {
     const li = document.createElement("li");
